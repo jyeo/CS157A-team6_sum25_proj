7ced85aeaada12c5595373545edb551211edee3b
“ambiguus <y03.jasp3r@gmail.com>
2025-07-06 00:45:55 -0700

    Updated main SQL file for team to update Added my own sections+DTD to cover: * User * Walk_Request * Walk_History * Security_Suite
    each section should have its own DTD
    
    ===========================
    | Edited on 05 July 2025. |
    | Added oon 05 July 2025. |
    |                         |
    | -Jasper Yeo             |
    | jasper.yeo@sjsu.edu     |
    ===========================

---
+++ code/hashmastr.java
@@ -0,0 +1,52 @@
+import java.security.SecureRandom;
+import java.util.Base64;
+import org.bouncycastle.crypto.generators.SCrypt;
+
+/*
+Link for code sources:
+https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+
+https://github.com/bcgit/bc-java/blob/main/core/src/main/java/org/bouncycastle/crypto/generators/SCrypt.java
+
+https://www.baeldung.com/java-secure-random
+https://www.baeldung.com/java-base64-encode-and-decode
+https://docs.oracle.com/javase/8/docs/api/java/util/Base64.html
+ */
+
+public class hashmastr {
+
+    //TESTER CLASS HERE
+    public class TestBOUNCYCASTLE {
+        public static void main(String[] args) {
+            System.out.println("BouncyCastle is online and running!");
+        }
+    }
+
+    public class ScryptReader {
+        public final String hash;
+        public final String salt;
+
+        public ScryptReader(String hash, String salt) {
+            this.hash = hash;
+            this.salt = salt;
+        }
+    }
+
+    public static String hashPSWD(String pswd) {
+        byte[] salt = new byte[16];
+        SecureRandom s_rand = new SecureRandom();
+        s_rand.nextBytes(salt);
+
+        int N = 16384;
+        int r = 32;
+        int p = 1;
+
+        byte[] hash = SCrypt.generate(pswd.getBytes(), salt, N, r, p, 32);
+
+        String hash_base64 = Base64.getEncoder().encodeToString(hash);
+        String salt_base64 = Base64.getEncoder().encodeToString(salt);
+
+        return new ScryptReader(hash_base64, salt_base64);
+    }
+
+} //end
