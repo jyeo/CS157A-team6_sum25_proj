03b0b7b16ea00ff21c818c94f57f5955c6e795c3
MadhavThankasala <151234661+MadhavThankasala@users.noreply.github.com>
2025-07-06 16:11:00 -0700

    Add files via upload

---
+++ code/Admin_Table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Admin(
+user_id INT PRIMARY KEY, password VARCHAR(45)
+);
+
+INSERT INTO Admin (user_id, password) VALUES
+(123, 'password1'),
+(321, 'password2'),
+(456, 'password3'),
+(654, 'password4'),
+(093, 'secure123'),
+(782, 'secure1'),
+(981, 'secure2'),
+(891, 'secure3'),
+(190, 'secure321'),
+(329, 'secure981');
